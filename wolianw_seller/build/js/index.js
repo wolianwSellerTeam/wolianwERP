1dc927b3db0fccd3298ca37717b54f00e0703e97
lihui <873095192@qq.com>
2017-09-16 00:24:31 +0800

    提交一个试试

---
+++ wolianw_seller/build/js/index.js
@@ -0,0 +1,13 @@
+
+/*var index;
+layui.config(['element', 'tab', 'navbar'], function(exports) {
+	
+});*/
+
+layui.define(['element', 'navbar'], function() {
+	var $ = layui.jquery,
+		layer = layui.layer;
+
+	
+	
+});
